e17c34628e5d23c2521ea40eead6bde35fb4145d
leahnjambi <njambileah07@gmail.com>
2023-02-27 10:28:31 +0300

    my registration form

---
+++ PyDjangoRegistration/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegistrationForm
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created successfully')
+            return redirect('users-registration')
+        else:
+            messages.error(request, 'Account created failed')
+            return redirect('users-registration')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'register.html', {'form': form})
