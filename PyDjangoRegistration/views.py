3f4bcfcaad6b50fbdee0aa0d476057bcedbfc23c
leahnjambi <njambileah07@gmail.com>
2023-02-28 10:28:40 +0300

    created a logging form

--- PyDjangoRegistration/views.py
+++ PyDjangoRegistration/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from .forms import UserRegistrationForm
+from django.contrib.auth.decorators import login_required
 
 
 def register(request):
@@ -17,3 +18,9 @@ def register(request):
     else:
         form = UserRegistrationForm()
     return render(request, 'register.html', {'form': form})
+
+
+
+@login_required
+def home(request):
+    return render(request, 'home.html')
